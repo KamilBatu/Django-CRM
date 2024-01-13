e39372e6ed1cd2fe75cdb8775b4d2996b503ebc9
KamilBatu <kh14053340@gmail.com>
2024-01-13 21:42:56 +0300

    third commit

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home  
+from .views import home, logout_user, register
 
 urlpatterns = [
-    path('', home, name='home')  
+    path('', home, name='home'),
+    # path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', register, name='register')
+
 ]
