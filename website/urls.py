33db57a23f74f65684723eaf7ce1237f36eb2040
KamilBatu <kh14053340@gmail.com>
2024-01-14 15:52:39 +0300

    final project

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import home, logout_user, register
+from .views import home, logout_user, register, record_customer, delete_record, addRecord, update
 
 urlpatterns = [
     path('', home, name='home'),
     # path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
-    path('register/', register, name='register')
-
+    path('register/', register, name='register'),
+    path('record/<int:pk>/', record_customer, name='record'),
+    path('delete/<int:pk>/', delete_record, name='delete'),
+    path('update/<int:pk>/', update, name='update'),
+    path('addRecord/', addRecord, name='addRecord')
 ]
--- website/views.py
+++ website/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .forms import AddRecordForm
+from .models import Record
+
+
 
 # Create your views here.
 
 def home(request):
+    records=Record.objects.all()
     if request.method=="POST":
         username=request.POST['username']
         password=request.POST['password']
@@ -16,12 +22,73 @@ def home(request):
         else:
             messages.error(request, 'something wrong please try again')
             return redirect('home')
-    return render(request, 'home.html')
+    else:
+        return render(request, 'home.html', {'records':records})
 # def login_user(request):
 #     pass
 def logout_user(request):
     logout(request)
     messages.success(request, 'you have logged out successfuly')
     return redirect('home')
+
+
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():  # Corrected the line here
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Successfully registered')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
+
+def record_customer(request, pk):
+
+    if request.user.is_authenticated:
+        record_customer=Record.objects.get(pk=pk)
+        return render(request, 'record.html', {"record_customer":record_customer})
+    else:
+        messages.success(request, 'you have to login frist')
+        return render(request, 'home.html')
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        customer=Record.objects.get(pk=pk)
+        customer.delete()
+        messages.success(request, 'record deleted successfuly')
+        return redirect('home')
+    else:
+        messages.error('you have to logged in to do that')
+        return redirect('home')
+    
+def addRecord(request):
+    form=AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method=='POST':
+            if form.is_valid:
+                form.save()
+                messages.success(request, 'added successfuly')
+                return redirect('home')
+        else:
+            return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.error(request, 'you need to login frist')
+        return redirect('home')
+def update(request, pk):
+    if request.user.is_authenticated:
+        current_record=Record.objects.get(pk=pk)
+        form=AddRecordForm(request.POST or None, instance=current_record)
+        if request.method=='POST':
+            if form.is_valid:
+                form.save()
+                messages.success(request, 'updated successfuly')
+                return redirect('home')
+        else:
+            return render(request, 'update.html', {'form':form})
+    else:
+        messages.error(request, 'you need to login frist')
+        return redirect('home')
